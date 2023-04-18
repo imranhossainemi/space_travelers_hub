8147c7cd3b82fbc206a734dcdc7e03f0fa20d064
Imran Hossain <blur-imran@hotmail.com>
2023-04-18 16:39:34 +0600

    Create MissionItems.js

---
+++ src/Components/MissionItems.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleJoinMission } from '../Features/Missions/missionsSlice';
+
+const MissionItems = (props) => {
+  const dispatch = useDispatch();
+  const joinMission = useSelector(
+    (state) => state.mission.joinMission[props.id]
+  );
+
+  const handleJoinMission = (missionId) => {
+    dispatch(toggleJoinMission({ missionId }));
+  };
+
+  const membershipStatusClassName = joinMission ? 'activeMember' : 'notMember';
+
+  const joinMissionStatusClassnName = joinMission
+    ? 'leaveMission'
+    : 'joinMission';
+
+  return (
+    <tr className="table-row">
+      <td className="mission-name">{props.name}</td>
+      <td>{props.description}</td>
+      <td>
+        <span className={`membership-status ${membershipStatusClassName}`}>
+          {joinMission ? 'Active Member' : 'NOT A MEMBER'}
+        </span>
+      </td>
+      <td>
+        <button
+          type="button"
+          onClick={() => handleJoinMission(props.id)}
+          className={`joinMission-action ${joinMissionStatusClassnName}`}
+        >
+          {joinMission ? 'Leave Mission' : 'Join Mission'}
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default MissionItems;
