0de1ccba81b42b44344e03ca33d183136e752755
byiringiroscar <byiringoroscar@gmail.com>
2023-04-18 15:44:48 +0200

    Add underline to every nav link clicked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
+import Mission from './components/mission/Mission';
+import Rocket from './components/rockets/Rocket';
+import Profile from './components/profile/Profile';
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Routes>
+        <Route path="/" element={<Rocket />} />
+        <Route path="/missions" element={<Mission />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
 import logo from './planet.png';
 
-const Header = () => (
-  <>
-    <header className="header">
-      <Link className="main-logo" to="/">
-        <img className="logo" src={logo} alt="Space Logo" />
-        <h3 className="logo-title">Space Travelers&rsquo; Hub</h3>
-      </Link>
-      <ul className="nav-links">
-        <li className="nav-link"><Link to="/">ROCKETS</Link></li>
-        <li className="nav-link"><Link to="/missions">MISSIONS</Link></li>
-        <li className="nav-link"><Link to="/profile">MY PROFILE</Link></li>
-      </ul>
-    </header>
-    <div className="divider">
-      <hr />
-    </div>
-  </>
-);
+const Header = () => {
+  const [activeLink, setActiveLink] = useState('rockets');
+
+  const handleClick = (link) => {
+    setActiveLink(link);
+  };
+
+  return (
+    <>
+      <header className="header">
+        <Link className="main-logo" to="/">
+          <img className="logo" src={logo} alt="Space Logo" />
+          <h3 className="logo-title">Space Travelers&rsquo; Hub</h3>
+        </Link>
+        <ul className="nav-links">
+          <li className={`nav-link ${activeLink === 'rockets' ? 'active-nav' : ''}`}>
+            <Link to="/" onClick={() => handleClick('rockets')}>
+              ROCKETS
+            </Link>
+          </li>
+          <li className={`nav-link ${activeLink === 'missions' ? 'active-nav' : ''}`}>
+            <Link to="/missions" onClick={() => handleClick('missions')}>
+              MISSIONS
+            </Link>
+          </li>
+          <li className={`nav-link ${activeLink === 'profile' ? 'active-nav' : ''}`}>
+            <Link to="/profile" onClick={() => handleClick('profile')}>
+              MY PROFILE
+            </Link>
+          </li>
+        </ul>
+      </header>
+      <div className="divider">
+        <hr />
+      </div>
+    </>
+  );
+};
 
 export default Header;
