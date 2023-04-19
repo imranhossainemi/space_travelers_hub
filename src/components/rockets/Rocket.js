efa5e4d406b16ac5a10356308da9bef130ce5026
byiringiroscar <byiringoroscar@gmail.com>
2023-04-19 20:11:49 +0200

    Add basic rocket setup

--- src/components/rockets/Rocket.js
+++ src/components/rockets/Rocket.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import './rocket.css';
+import Card from './Card';
 
 const Rocket = () => (
-  <div>Rocket</div>
+  <section className="rocket">
+    <div className="card-container">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </section>
 );
 
 export default Rocket;
