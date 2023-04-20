9ee40acf03da725297e40b946998c0989497b1af
byiringiroscar <byiringoroscar@gmail.com>
2023-04-20 12:35:32 +0200

    Add filter reserved space

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import MissionJoin from '../mission/MissionJoin';
+import './Profile.css';
+// import ProfileRocket from './ProfileRocket';
 
 const Profile = () => (
   <div className="main-container">
     <MissionJoin />
+    {/* <ProfileRocket /> */}
   </div>
 );
 
