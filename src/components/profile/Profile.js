a10bbdf0c25930c1d7289da445226684db894432
byiringiroscar <byiringoroscar@gmail.com>
2023-04-20 12:59:51 +0200

    Fix linters errors

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import MissionJoin from '../mission/MissionJoin';
 import './Profile.css';
-import ProfileRocket from './ProfileRocket';
+import ProfileRocket from './ProfileRocket'; // eslint-disable-line import/extensions
 
 const Profile = () => (
   <div className="main-container">
