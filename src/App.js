b4c4509c18c1baf54067bcdc9bd5d1fe7bf2c5d0
byiringiroscar <byiringoroscar@gmail.com>
2023-04-20 12:55:56 +0200

    Add reserved ship in profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Header from './components/header/Header';
 import Missions from './components/mission/Missions';
 import Rocket from './components/rockets/Rocket';
 import Profile from './components/profile/Profile';
+import { fetchRockets } from './Features/Rockets/rocketsSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
   return (
     <div className="container">
       <Header />
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import MissionJoin from '../mission/MissionJoin';
 import './Profile.css';
-// import ProfileRocket from './ProfileRocket';
+import ProfileRocket from './ProfileRocket';
 
 const Profile = () => (
   <div className="main-container">
     <MissionJoin />
-    {/* <ProfileRocket /> */}
+    <ProfileRocket />
   </div>
 );
 
